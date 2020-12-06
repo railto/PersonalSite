2d8f0b74ba0b85abbdc3a985c5e373a8c8cfc970
Mark Railton <mark@markrailton.com>
2020-12-06 17:55:16 +0000

    Removes default controller and views

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,3 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::get('/', 'App\Http\Controllers\PagesController@index')->name('index');
 Route::get('/uses', 'App\Http\Controllers\PagesController@uses')->name('uses');
-
-Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
