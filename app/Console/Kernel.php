df3c9b2902ccecadb36c5a78722dc19dd093f539
Mark Railton <mark@markrailton.com>
2020-12-16 20:35:59 +0000

    adds schedule for backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('backup:run')->dailyAt('01:00')->evenInMaintenanceMode();
         $schedule->command('twitter:update')->everyFourHours()->evenInMaintenanceMode();
     }
 
