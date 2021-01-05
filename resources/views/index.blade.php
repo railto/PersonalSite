8ce623c283b17314ffacfb3c5e1307f364045c45
Mark Railton <mark@markrailton.com>
2021-01-05 18:06:13 +0000

    update index text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,25 +16,23 @@
         <div class="max-w-5xl px-6 mx-auto text-center">
             <h2 class="text-2xl font-semibold text-white">About Me</h2>
             <p class="text-white mt-4">
-                At the end of 2013 I got bitten by the bug that is web development and ever
-                since I've been hooked. I love improving my programming abilities and am generally willing to try
-                anything twice (within reason).
+                At the end of 2013 I got bitten by the bug that is web development and ever since I've been hooked. I
+                love improving my programming abilities and am generally willing to try anything twice (within reason).
             </p>
 
             <p class="text-white mt-4">
-                Over the years I've worked with a few different programming languages, namely
-                PHP, JavaScript, TypeScript and more recently Python. I'm most comfortable building back end server
-                based systems, but over the last couple of years I've also been experimenting more with front end
-                development work and thanks to the React library I'm starting to feel a bit more comfortable on the
-                front end of things as well.
+                Over the years I've worked with a few different programming languages, namely PHP, JavaScript,
+                TypeScript and also some Python. I'm most comfortable building back end server based systems, but over
+                the last couple of years I've also been experimenting more with front end development work and thanks to
+                the React and Vue libraries I'm starting to feel a bit more comfortable on the front end of things as
+                well.
             </p>
 
             <p class="text-white mt-4">
-                If you've visited this site before, you'll likely know that I tend to use it
-                as a bit of a playground. Currently, it's built using Symfony, a powerful PHP Framework. The front end
-                styling
-                is done using Tailwind CSS, an excellent utility based CSS framework that's making it
-                a bit easier to get a good design going.
+                If you've visited this site before, you'll likely know that I tend to use it as a bit of a playground.
+                Currently, it's built using Laravel, a powerful PHP Framework. The front end styling is done using
+                Tailwind CSS, an excellent utility based CSS framework that's making it a bit easier to get a good
+                design going. I've also managed to sprinkle in some JavaScript thanks to the Vue library.
             </p>
         </div>
     </section>
@@ -72,5 +70,5 @@
             </div>
         </div>
     </section>
-    <newsletter-subscribe />
+    <newsletter-subscribe/>
 @endsection()
