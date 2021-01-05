9e8a151eb1ba94581b5046ff4f61c3b9df486597
Mark Railton <mark@markrailton.com>
2021-01-05 17:05:06 +0000

    Add back support for vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,13 @@
 require('alpinejs');
 import Prism from 'prismjs';
+
+window.Vue = require('vue');
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+const app = new Vue({
+    el: '#app',
+});
