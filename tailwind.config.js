8348825bca8f47b73acf0c5c5c8700173962c350
Mark Railton <mark@markrailton.com>
2021-01-05 18:02:33 +0000

    helps to check vue files for css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
       './resources/views/**/*.blade.php',
       './resources/css/**/*.css',
+      './resources/js/**/*.vue',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
