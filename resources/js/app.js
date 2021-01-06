5a0694b10cab4626232581a4cf902151e74b878d
Mark Railton <mark@markrailton.com>
2021-01-06 13:31:01 +0000

    Move App components to subdir

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,9 +6,8 @@ window.Vue = Vue;
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-const components = require.context('./', true, /\.vue$/i)
+const components = require.context('./App', true, /\.vue$/i)
 components.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], components(key).default))
-
 
 const app = new Vue({
     el: '#app',
