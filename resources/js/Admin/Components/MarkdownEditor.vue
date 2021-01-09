f1ebe3f122af54ee06adfd98b5f9d7a0f46f2877
Mark Railton <mark@markrailton.com>
2021-01-09 16:25:47 +0000

    adds tabs for markdown editor

--- resources/js/Admin/Components/MarkdownEditor.vue
+++ resources/js/Admin/Components/MarkdownEditor.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <textarea
-            class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-            :value="value"
-            @input="$emit('input', $event.target.value); resize($event)"
-            :id="label"
-        ></textarea>
-        <div v-html="compiledMarkdown"></div>
+        <tabs>
+            <tab name="Editor">
+               <textarea
+                   class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                   :value="value"
+                   @input="$emit('input', $event.target.value); resize($event)"
+                   :id="label"
+               ></textarea>
+            </tab>
+            <tab name="Preview">
+                <div v-html="compiledMarkdown"></div>
+            </tab>
+        </tabs>
     </div>
 </template>
 
 <script>
 import marked from 'marked';
+import {Tabs, Tab} from 'vue-tabs-component';
 
 export default {
     name: 'MarkdownEditor',
@@ -23,6 +30,10 @@ export default {
             required: false,
         },
     },
+    components: {
+        Tab,
+        Tabs,
+    },
     methods: {
         resize(e) {
             e.target.style.height = 'auto';
@@ -30,7 +41,7 @@ export default {
         },
     },
     computed: {
-        compiledMarkdown: function() {
+        compiledMarkdown: function () {
             return marked(this.value, {sanitise: true});
         },
     },
