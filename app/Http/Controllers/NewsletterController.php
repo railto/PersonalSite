124d283ee3234376f610c3a589a9b1ff4c3cbb26
Mark Railton <mark@markrailton.com>
2021-01-17 20:47:50 +0000

    queue newsletter subscription

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -9,10 +9,12 @@ class NewsletterController extends Controller
 {
     public function subscribe(Request $request)
     {
-        Http::withToken(env('SENDPORAL_API_KEY'))->post(env('SENDPORTAL_API_URL') . '/workspaces/' . env('SENDPORTAL_WORKSPACE_ID') . '/subscribers', [
-            'first_name' => $request->first_name,
-            'email' => $request->email,
-        ]);
+        $this->dispatch(function () use ($request) {
+            Http::withToken(env('SENDPORAL_API_KEY'))->post(env('SENDPORTAL_API_URL') . '/workspaces/' . env('SENDPORTAL_WORKSPACE_ID') . '/subscribers', [
+                'first_name' => $request->get('first_name'),
+                'email' => $request->get('email'),
+            ]);
+        });
 
         return response()->json(['status' => 'success']);
     }
