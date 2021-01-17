21cf7cb03a79e3e9417da78bd04e081688f0bae7
Mark Railton <mark@markrailton.com>
2021-01-17 21:51:05 +0000

    some updates for markdown editor

--- resources/js/Admin/Components/MarkdownEditor.vue
+++ resources/js/Admin/Components/MarkdownEditor.vue
@@ -3,7 +3,7 @@
         <tabs>
             <tab name="Editor">
                <textarea
-                   class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+                   class="no-resize appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
                    :value="value"
                    @input="$emit('input', $event.target.value); resize($event)"
                    :id="label"
@@ -42,7 +42,10 @@ export default {
     },
     computed: {
         compiledMarkdown: function () {
-            return marked(this.value, {sanitise: true});
+            return marked(this.value, {
+                gfm: true,
+                breaks: true,
+            });
         },
     },
 };
